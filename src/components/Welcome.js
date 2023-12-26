a6508012f9eaf01f2b2d4c0c5cf7f3e1363cf627
aditya4-4 <adityadurgesh4@gmail.com>
2023-12-26 22:18:15 +0530

    submitting code-Tue Dec 26 2023 22:18:13 GMT+0530 (India Standard Time)

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,1 +1,13 @@
 // write code for Welcome component here
+import React from 'react'
+
+function Welcome({name}) {
+  return (
+    <div>
+        <h1>Hey! {name}</h1>
+        <p>Welcome to School.</p>
+    </div>
+  )
+}
+
+export default Welcome
